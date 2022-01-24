289f61ffcc0e20e92ba88efdeb527584bef14811
Scott Stafford <scott.stafford@databricks.com>
2022-01-24 14:38:19 -0500

    Add processing instruction

--- jazz/data.py
+++ jazz/data.py
@@ -4,11 +4,14 @@ import random
 from pathlib import Path
 import time
 
+def get_processing_instruction():
+    pi = ET.ProcessingInstruction("xml", "version='1.0' encoding='UTF-8'")
+    return ET.tostring(pi).decode("UTF-8")
+
 def generate_xml() -> str:
     faker = Faker()
-    pi = ET.ProcessingInstruction("version", "1.0")
     root = ET.Element('MESSAGE')
-    ET.PI(pi)
+    
     metadata = ET.SubElement(root, 'METADATA')
     original_date = ET.SubElement(metadata, "ORIGINAL_DATE")
     original_date.text = faker.date()
@@ -25,11 +28,11 @@ def generate_xml() -> str:
     # ET.indent(root) 
 
     # create a new XML file with the results
-    return ET.tostring(root)
+    return get_processing_instruction() + ET.tostring(root).decode("UTF-8")
 
 def save_file(path, data):
     myfile = open(path, "w")
-    myfile.write(data.decode('UTF-8'))
+    myfile.write(data)
 
 def generate(path, number_files):
     path = Path(path)
--- tests/unit/test_xml.py
+++ tests/unit/test_xml.py
@@ -1,8 +1,9 @@
-from jazz.data import generate_xml, generate
+from jazz.data import generate_xml, generate, get_processing_instruction
 from pathlib import Path
 
 def test_xml_file_exists(tmpdir):
     xml_string = generate_xml()
+    print(xml_string)
     assert(xml_string.startswith("<?xml version='1.0'"))
     assert(xml_string.endswith("</MESSAGE>"))
     
@@ -13,4 +14,7 @@ def test_generate_files(tmpdir):
         xml_path = tmpdir + "/sample-" + str(i) + ".xml"
         path = Path(xml_path)
         assert(path.is_file())
-    
+
+
+def test_processing_instruction():
+    print(get_processing_instruction())
