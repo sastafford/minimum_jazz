cb828b65da18aea936a662ef6ba2ea1999950bd3
Scott Stafford <scott.stafford@databricks.com>
2022-01-24 10:24:00 -0500

    Add random data to xml

--- jazz/xml.py
+++ jazz/xml.py
@@ -1,17 +1,21 @@
 from faker import Faker
 import xml.etree.ElementTree as ET
-
-def get_random_name():
-    faker = Faker()
-    return faker.name()
+import random
 
 def generate_xml(path):
+    faker = Faker()
     root = ET.Element('MESSAGE')
     metadata = ET.SubElement(root, 'METADATA')
-    delivery_date = ET.SubElement(root, 'DeliveredDate')
+    original_date = ET.SubElement(metadata, "ORIGINAL_DATE")
+    original_date.text = faker.date()
+    delivery_date = ET.SubElement(metadata, 'DELIVERED_DATE')
+    delivery_date.text = faker.date()
+    classification = ET.SubElement(metadata, "CLASSIFICATION")
+    classification.text = random.choice(["OPEN", "SENSITIVE", "PROPRIETARY"])
     subject = ET.SubElement(root, 'SUBJECT')
+    subject.text = faker.sentence()
     body = ET.SubElement(root, "BODY")
-    classification = ET.SubElement(root, "CLASSIFICATION")
+    body.text = faker.paragraph(nb_sentences=10)
     ET.indent(root)
 
     # create a new XML file with the results
