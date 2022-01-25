84834950d04cc3c94bdd0b737bc3c8c7cd5b523f
Scott Stafford <scott.stafford@databricks.com>
2022-01-24 22:38:22 -0500

    Testing parse_xml udf

--- jazz/pipeline.py
+++ jazz/pipeline.py
@@ -1,5 +1,6 @@
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import udf, col
+from pyspark.sql.types import StructField, StructType, StringType, MapType
 
 from typing import Dict
 import xml.etree.ElementTree as ET
@@ -9,13 +10,19 @@ def to_bronze(spark, raw_path: str) -> DataFrame:
     return df
 
 def to_silver(spark, bronze_df: DataFrame) -> DataFrame:
-    parseXml = udf(parse_xml)
-    df = bronze_df.select("value", parseXml(col("value")).alias("message"))
+    df = bronze_df.select("value", parse_xml(col("value")).alias("message"))
     return df
 
+def _parse_xml_struct_schema():
+    return StructType([
+            StructField('subject', StringType(), nullable=True),
+            StructField('body', StringType(), nullable=True)
+        ])
+
+@udf(returnType=_parse_xml_struct_schema())
 def parse_xml(xml_string: str) -> Dict:
     values = {}
     root = ET.fromstring(xml_string)
-    values["SUBJECT"] = root.findall(".//SUBJECT")[0].text
-    values["BODY"] = root.findall(".//BODY")[0].text
+    values["subject"] = root.findall(".//SUBJECT")[0].text
+    values["body"] = root.findall(".//BODY")[0].text
     return values
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,7 +1,8 @@
 from jazz.data import generate, generate_xml
 from jazz.pipeline import to_bronze, to_silver, parse_xml
 
-from pathlib import Path
+from pyspark.sql import Row
+from pyspark.sql.functions import col
 
 def test_to_bronze(tmpdir, spark):
     path = str(tmpdir)
@@ -21,18 +22,26 @@ def test_to_silver(tmpdir, spark):
     generate(raw_path, 3)
     bronze_df = to_bronze(spark, raw_path)
     silver_df = to_silver(spark, bronze_df)
-    fields = silver_df.schema.fieldNames()
+    schema = silver_df.schema
+    fields = schema.fieldNames()
+    print(schema)
     print(fields)
     assert("value" in fields)
     assert("message" in fields)
     silver_df.show()
 
 
-def test_parse_xml():
+def test_parse_xml(spark):
     xml_string = generate_xml()
-    values = parse_xml(xml_string)
-    print(values)
-    assert("SUBJECT" in values.keys())
-    assert("BODY" in values.keys())
-    subject = values.get("SUBJECT")
-    assert(len(subject) > 10)
+    df = spark.createDataFrame([
+        Row(value = xml_string)
+    ])
+    df = df.select(parse_xml(col("value")).alias("message"))
+    schema = df.schema
+    fields = schema.fieldNames()
+    df = df.select("message.subject", "message.body")
+    fields = df.schema.fieldNames()
+    assert("subject" in fields)
+    assert("body" in fields)
+    values = df.collect()
+    assert(len(values[0][0]) > 10)
