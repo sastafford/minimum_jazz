05ebef2dca010125c0147d059169fd11deac06a1
Scott Stafford <scott.stafford@databricks.com>
2022-02-27 23:38:15 -0500

    Different directories between spark driver and dbfs

--- jazz/jobs/bronze.py
+++ jazz/jobs/bronze.py
@@ -7,12 +7,14 @@ from jazz.helper import get_spark
 
 p = ArgumentParser()
 p.add_argument("--number_files", required=False, type=int, default=100)
+p.add_argument("--xml_directory", required=False, type=str, default="/tmp/minimum_jazz/raw")
 p.add_argument("--project_home", required=False, type=str, default="/dbfs/Shared/minimum_jazz/")
 namespace = p.parse_known_args(sys.argv[1:])[0]
 
-raw_dir = namespace.project_home + "/raw"
-generate(raw_dir, namespace.number_files)
+generate(namespace.xml_directory, namespace.number_files)
 
+raw_dir = namespace.project_home + "/raw"
 bronze_dir = namespace.project_home + "/bronze"
 df = to_bronze(get_spark(), raw_dir)
+print(bronze_dir)
 df.write.format("delta").save(bronze_dir)
